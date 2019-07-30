7b4d6d7519d54e67b5765f1a145b97af0914cfcb
Dan Dixey <dan.dixey@gmail.com>
2019-07-30 22:24:34 +0100

    Remove unused files

--- src/main/scala/com/dandxy/db/Migration.scala
+++ src/main/scala/com/dandxy/db/Migration.scala
@@ -19,7 +19,7 @@ object Migration extends LazyLogging {
       val migrationsApplied: Int = fw.migrate()
 
       logger.info(
-        s"Successfully applied $migrationsApplied migrations to the `entitlements` database"
+        s"Successfully applied $migrationsApplied migrations to the database"
       )
       migrationsApplied
     }.leftMap {
