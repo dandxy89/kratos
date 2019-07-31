7a5b26f5eb55bb84cceafc08c60170ec13f9d18d
Dan Dixey <dan.dixey@gmail.com>
2019-07-31 17:52:13 +0100

    Better Temperature

--- src/main/scala/com/dandxy/model/golf/input/Temperature.scala
+++ src/main/scala/com/dandxy/model/golf/input/Temperature.scala
@@ -1,3 +1,17 @@
 package com.dandxy.model.golf.input
 
-final case class Temperature(value: Double)
+sealed trait Temperature {
+  def toCelsius: Int
+}
+
+object Temperature {
+
+  final case class Celsius(value: Int) extends Temperature {
+    def toCelsius: Int = value
+  }
+
+  final case class Fahrenheit(value: Int) extends Temperature {
+    // (T(°F) - 32) / (9 / 5)
+    def toCelsius: Int = ((value - 32) / (9.0 / 5)).toInt
+  }
+}
