6d46c5d5577f988c2d1a72778a1495926f2493db
Dan Dixey <dan.dixey@gmail.com>
2019-08-05 12:53:13 +0100

    Prep work for IT tests

---
+++ src/it/scala/com/dandxy/db/PGAQueryToolSpec.scala
@@ -0,0 +1,26 @@
+package com.dandxy.db
+
+import com.dandxy.db.HealthCheck.OK
+import com.dandxy.util.PostgresDockerService
+import org.scalatest.concurrent.Eventually
+import org.scalatest.{ BeforeAndAfterAll, FlatSpec, Matchers }
+
+class PGAQueryToolSpec extends FlatSpec with Matchers with Eventually with BeforeAndAfterAll {
+
+  val postgresDockerService = new PostgresDockerService(5445)
+
+  eventually(postgresDockerService.isPostgresRunning shouldBe true)
+
+  override def beforeAll(): Unit = Migration.flywayMigrateDatabase(postgresDockerService.config) match {
+    case Left(error) => fail(error.getMessage)
+    case Right(_)    => assert(true)
+  }
+
+  "HealthCheck" should "return the Status of the Database OK if it has started correctly" in {
+    HealthCheck.queryStatus(postgresDockerService.postgresTransactor).unsafeRunSync() shouldBe OK
+  }
+
+  "PGAQueryTool" should "pending" in {
+    pending
+  }
+}
