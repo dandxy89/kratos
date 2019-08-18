2ea6513cf683109b9ceb9f371b15bb82d482a78b
Dan Dixey <dan.dixey@gmail.com>
2019-08-18 09:08:00 +0100

    Adding middleware implementation

---
+++ src/main/scala/com/dandxy/middleware/instances/EntityInstances.scala
@@ -0,0 +1,23 @@
+package com.dandxy.middleware.instances
+
+import com.dandxy.model.http.{ IsRequest, IsResponse }
+import org.http4s.{ Request, Response }
+import org.http4s.util.CaseInsensitiveString
+
+import scala.language.higherKinds
+
+trait EntityInstances {
+
+  implicit def isRequest[F[_]]: IsRequest[Request[F]] = new IsRequest[Request[F]] {
+    override def headerByName(value: Request[F], name: String): Option[String] =
+      value.headers.get(CaseInsensitiveString(name)).map(_.value)
+    override def method(value: Request[F]): String              = value.method.name
+    override def path(value: Request[F]): String                = value.uri.path
+    override def params(value: Request[F]): Map[String, String] = value.params
+  }
+
+  implicit def isResponse[F[_]]: IsResponse[Response[F]] = new IsResponse[Response[F]] {
+    override def status(value: Response[F]): Int                 = value.status.code
+    override def contentLength(value: Response[F]): Option[Long] = value.contentLength
+  }
+}
---
+++ src/main/scala/com/dandxy/middleware/instances/ToResponseInstances.scala
@@ -0,0 +1,19 @@
+package com.dandxy.middleware.instances
+
+import cats.Monad
+import cats.data.OptionT
+import cats.syntax.all._
+import com.dandxy.middleware.ToResponse
+import com.dandxy.middleware.implementation.http4s.ToHttpResponse
+import org.http4s.{Response, Status}
+
+import scala.language.higherKinds
+
+trait ToResponseInstances {
+  implicit def unitResponse[F[_]: Monad]: ToHttpResponse[F, Unit] = ToResponse.pure(Response(Status.NoContent))
+
+  implicit def optionResponse[F[_]: Monad, T](implicit encoder: ToHttpResponse[F, T]): ToHttpResponse[F, Option[T]] =
+    ToResponse.instance { (media, value) =>
+      value.fold(Response[F](Status.NotFound).pure[OptionT[F, ?]])(encoder.run(media))
+    }
+}
