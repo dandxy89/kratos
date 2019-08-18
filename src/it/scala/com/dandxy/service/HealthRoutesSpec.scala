e7453394fdd0a288d59e2ecdebd1e435dfe552df
Dan Dixey <dan.dixey@gmail.com>
2019-08-18 13:39:54 +0100

    Updating tests

--- src/it/scala/com/dandxy/service/HealthRoutesSpec.scala
+++ src/it/scala/com/dandxy/service/HealthRoutesSpec.scala
@@ -1,27 +1,54 @@
 package com.dandxy.service
 
 import cats.effect.IO
-import org.http4s.{ Header, Headers, HttpVersion, Method, Request, Uri }
-import org.scalatest.{ FlatSpec, Matchers }
+import cats.effect.concurrent.Ref
+import com.dandxy.db.util.HealthCheck.{ OK, Status, Warning }
+import com.dandxy.util.PostgresDockerService
+import org.http4s.{ EntityDecoder, Header, Headers, Method, Request, Response, Uri }
+import org.scalatest.{ Assertion, FlatSpec, Matchers }
 
 class HealthRoutesSpec extends FlatSpec with Matchers {
 
+  val service = new PostgresDockerService(5450)
+
   behavior of "Health Routes"
 
-  val endpointHealth: HealthRoutes[IO] = HealthRoutes[IO]()
+  def endpointHealth(dbStatus: IO[Ref[IO, Status]]): HealthRoutes[IO] =
+    HealthRoutes[IO](dbStatus.unsafeRunSync())
+
+  def responseMatcher[A](r: Option[Response[IO]], expected: A)(implicit ee: EntityDecoder[IO, A]): Assertion =
+    r match {
+      case Some(value) => value.as[A].unsafeRunSync() shouldBe expected
+      case None        => fail("Failed to get correct response from route")
+    }
+
+  val goodHealth: IO[Ref[IO, Status]] = Ref[IO].of[Status](OK)
+  val badHealth: IO[Ref[IO, Status]]  = Ref[IO].of[Status](Warning)
 
   it should "respond with Pong" in {
     val request = Request[IO](Method.GET, Uri.unsafeFromString("/ping"))
       .withHeaders(Headers.of(Header("Accept", "text/plain")))
 
-    val resp = endpointHealth.pingPongService
+    val resp = endpointHealth(goodHealth).healthService
       .run(request)
       .value
-      .unsafeRunSync()
 
-    resp match {
-      case Some(value) => value.as[String].unsafeRunSync() shouldBe "Pong"
-      case None        => fail("Failed to get correct response from ping route")
-    }
+    responseMatcher[String](resp.unsafeRunSync(), "Pong")
+  }
+
+  it should "respond correctly when checking the db status" in {
+    val request = Request[IO](Method.GET, Uri.unsafeFromString("/db/status"))
+      .withHeaders(Headers.of(Header("Accept", "text/plain")))
+
+    val goodResp = endpointHealth(goodHealth).healthService
+      .run(request)
+      .value
+
+    val badResp = endpointHealth(badHealth).healthService
+      .run(request)
+      .value
+
+    responseMatcher[String](goodResp.unsafeRunSync(), """{"postgres":{"status":"Ok"}}""")
+    responseMatcher[String](badResp.unsafeRunSync(), """{"postgres":{"status":"Warning"}}""")
   }
 }
