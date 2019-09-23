70da803b48802943cc4634cdae92a89cd6c49ed4
Dan Dixey <dan.dixey@gmail.com>
2019-09-23 17:49:44 +0100

    Preparing the Golf Routes for tests

--- src/it/scala/com/dandxy/service/GolferRoutesSpec.scala
+++ src/it/scala/com/dandxy/service/GolferRoutesSpec.scala
@@ -1,60 +1,25 @@
-// package com.dandxy.service
+package com.dandxy.service
 
-// import java.sql.Timestamp
+import cats.effect._
+import com.dandxy.jwt.GenerateToken
+import com.dandxy.model.player.PlayerId
+import com.dandxy.testData.MockRouteTestData
+import org.scalatest.{FlatSpec, Matchers}
 
-// import cats.effect.IO
-// import com.dandxy.jwt.GenerateToken
-// import com.dandxy.model.player.PlayerId
-// import org.scalatest.FlatSpec
-// import com.dandxy.golf.entity.Location
-// import com.dandxy.golf.input.Distance
-// import com.dandxy.golf.pga.Statistic.PGAStatistic
-// import com.dandxy.db.UserStore
-// import cats.effect.Bracket
-// import com.dandxy.auth.PlayerHash
-// import com.dandxy.auth.PasswordAuth.{ hashPassword, verifyPassword }
-// import com.dandxy.config.AuthSalt
-// import com.dandxy.db.sql.TableName._
-// import com.dandxy.golf.input.GolfInput.{ UserGameInput, UserShotInput }
-// import com.dandxy.golf.input.{ Handicap, HandicapWithDate }
-// import com.dandxy.model.player.PlayerId
-// import com.dandxy.model.user.Identifier.{ GameId, Hole }
-// import com.dandxy.model.user._
-// import com.dandxy.strokes.GolfResult
-// import doobie.implicits._
-// import doobie.util.transactor.Transactor
+import scala.concurrent.ExecutionContext
 
-// class GolferRoutesSpec extends FlatSpec {
+class GolferRoutesSpec extends FlatSpec with Matchers with MockRouteTestData {
 
-//   behavior of "Golf Routes"
+  behavior of "Golf Routes"
 
-//   val testKey                                                            = "test_secret_key"
-//   val validToken: String                                                 = GenerateToken.prepareToken(1, testKey)(PlayerId(1))
-//   val mockGetStatistic: (Distance, Location) => IO[Option[PGAStatistic]] = ???
+  val testKey            = "test_secret_key"
+  val validToken: String = GenerateToken.prepareToken(1, testKey)(PlayerId(1))
 
-//   val mockUserStore = new UserStore[IO] {
-//     def gdprPurge(playerId: PlayerId): IO[Int]                                                                      = ???
-//     def registerUser(registration: UserRegistration, hashedPassword: Password, updateTime: Timestamp): IO[PlayerId] = ???
-//     def attemptLogin(email: UserEmail, rawPassword: Password): IO[Option[PlayerId]]                                 = ???
-//     def addClubData(playerId: PlayerId, input: List[GolfClubData]): IO[Int]                                         = ???
-//     def getUserClubs(playerId: PlayerId): IO[List[GolfClubData]]                                                    = ???
-//     def getAllPlayerGames(playerId: PlayerId): IO[List[UserGameInput]]                                              = ???
-//     def getPlayerGame(gameId: GameId): IO[Option[UserGameInput]]                                                    = ???
-//     def addPlayerGame(game: UserGameInput): IO[GameId]                                                              = ???
-//     def deletePlayerGame(gameId: GameId): IO[Int]                                                                   = ???
-//     def dropByHole(gameId: GameId, hole: Hole): IO[Int]                                                             = ???
-//     def addPlayerShots(input: List[UserShotInput]): IO[Int]                                                         = ???
-//     def getByGameAndMaybeHole(gameId: GameId, hole: Option[Hole]): IO[List[UserShotInput]]                          = ???
-//     def getHandicapHistory(playerId: PlayerId): IO[List[HandicapWithDate]]                                          = ???
-//     def aggregateGameResult(gameId: GameId): IO[List[AggregateGameResult]]                                          = ???
-//     def addResultByIdentifier(result: GolfResult, h: Option[Hole]): IO[Int]                                         = ???
-//     def getResultByIdentifier(game: GameId, h: Option[Hole]): IO[Option[GolfResult]]                                = ???
-//     def getGameHandicap(game: GameId): IO[Option[Handicap]]                                                         = ???
-//   }
+  implicit private val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
 
-//   val golfingRoute = GolferRoutes[IO](mockUserStore, testKey, mockGetStatistic).golferRoutes
+  val golfingRoute = GolferRoutes[IO](mockStore, testKey, mockStat).golferRoutes
 
-//   it should "add clubs to the db" in {
-//     pending
-//   }
-// }
+  it should "add clubs to the db" in {
+    pending
+  }
+}
