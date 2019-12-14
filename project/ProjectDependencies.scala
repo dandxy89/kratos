70798ea20acc19d11fa6df052f6e0be063c53114
Dan Dixey <dan.dixey@gmail.com>
2019-12-14 21:44:35 +0000

    Updating ZIO

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -11,10 +11,9 @@ object ProjectDependencies extends {
     val logbackVersion     = "1.2.3"
     val scalaLogVersion    = "3.9.2"
     val pureConfigVersion  = "0.10.2"
-    val zioVersion         = "1.0-RC4"
+    val zioVersion         = "1.0.0-RC17"
     val scalaTestVersion   = "3.0.5"
     val flywayVersion      = "6.1.0"
-    val refindedVersion    = "0.9.8"
     val profigVersion      = "2.3.4"
     val scala4jLogging     = "1.7.26"
     val circeConfigVersion = "0.6.1"
@@ -23,23 +22,20 @@ object ProjectDependencies extends {
 
   def apply(): Seq[ModuleID] = Seq(
     // Project dependencies
-    "co.fs2"                     %% "fs2-core"                % Version.fS2Version,
-    "co.fs2"                     %% "fs2-io"                  % Version.fS2Version,
-    "io.circe"                   %% "circe-config"            % Version.circeConfigVersion,
-    "org.tpolecat"               %% "doobie-core"             % Version.doobieVersion,
-    "org.tpolecat"               %% "doobie-hikari"           % Version.doobieVersion,
-    "org.tpolecat"               %% "doobie-postgres"         % Version.doobieVersion,
-    "org.tpolecat"               %% "doobie-scalatest"        % Version.doobieVersion,
-    "org.flywaydb"               % "flyway-core"              % Version.flywayVersion,
-    "org.slf4j"                  % "slf4j-log4j12"            % Version.scala4jLogging,
-    "com.typesafe.scala-logging" %% "scala-logging"           % Version.scalaLogVersion,
-    "com.github.pureconfig"      %% "pureconfig"              % Version.pureConfigVersion,
-    "com.github.pureconfig"      %% "pureconfig-cats-effect"  % Version.pureConfigVersion,
-    "eu.timepit"                 %% "refined"                 % Version.refindedVersion,
-    "eu.timepit"                 %% "refined-cats"            % Version.refindedVersion,
-    "org.scalaz"                 %% "scalaz-zio"              % Version.zioVersion,
-    "org.scalaz"                 %% "scalaz-zio-interop-cats" % Version.zioVersion,
-    "org.scalatest"              %% "scalatest"               % Version.scalaTestVersion % "it, test",
+    "co.fs2"                     %% "fs2-core"               % Version.fS2Version,
+    "co.fs2"                     %% "fs2-io"                 % Version.fS2Version,
+    "io.circe"                   %% "circe-config"           % Version.circeConfigVersion,
+    "org.tpolecat"               %% "doobie-core"            % Version.doobieVersion,
+    "org.tpolecat"               %% "doobie-hikari"          % Version.doobieVersion,
+    "org.tpolecat"               %% "doobie-postgres"        % Version.doobieVersion,
+    "org.tpolecat"               %% "doobie-scalatest"       % Version.doobieVersion,
+    "org.flywaydb"               % "flyway-core"             % Version.flywayVersion,
+    "org.slf4j"                  % "slf4j-log4j12"           % Version.scala4jLogging,
+    "com.typesafe.scala-logging" %% "scala-logging"          % Version.scalaLogVersion,
+    "com.github.pureconfig"      %% "pureconfig"             % Version.pureConfigVersion,
+    "com.github.pureconfig"      %% "pureconfig-cats-effect" % Version.pureConfigVersion,
+    "dev.zio"                    %% "zio"                    % Version.zioVersion,
+    "org.scalatest"              %% "scalatest"              % Version.scalaTestVersion % "it, test",
     // Kratos Libraries
     "com.dandxy89" %% "kratos-middleware" % Version.kratosLibVersion,
     "com.dandxy89" %% "kratos-auth"       % Version.kratosLibVersion,
